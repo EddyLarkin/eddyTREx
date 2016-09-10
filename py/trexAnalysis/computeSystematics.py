74aa0989253295e358dae68d04ed74f7fb1ffb36
Edward Larkin <E.J.P.Larkin@warwick.ac.uk>
2016-09-10 11:13:22 +0100

    Code for systematics evaluation for other variables

--- py/trexAnalysis/computeSystematics.py
+++ py/trexAnalysis/computeSystematics.py
@@ -29,6 +29,8 @@ DIRS = (  RUN2WATER,
 TREE_SYS = ( "all_syst" )
 
 BINS_MOM = (0., 200., 400., 600., 800., 1000., 1200., 1400., 1600., 1800., 2000.)
+BINS_COSTHETA = (0., .1, .2, .3, .4, .5, .6, .7, .8, .9, 1.001)
+BINS_MULT = (0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
 
 def main(args):
   ROOT.gROOT.SetBatch(1)
@@ -38,6 +40,14 @@ def main(args):
     getSingleBinSyst(args)
   if args.momBin:
     getMomBinSyst(args)
+  if args.costhetaBin:
+    getCosThetaBinSyst(args)
+  if args.hotPathBin:
+    getHotPathBinSyst(args)
+  if args.hotProtonBin:
+    getHotProtonBinSyst(args)
+  if args.maxProtonMomBin:
+    getMaxProtonMomBinSyst(args)
 
 def getSingleBinSyst(args):
   fileSets = getFileSets(args)
@@ -49,7 +59,6 @@ def getSingleBinSyst(args):
     for runName, runFiles in fileSet.iteritems():
       for file in runFiles:
         bigTree.Add(file)
-        break
 
     print "  {0} events total".format(bigTree.GetEntries())
 
@@ -123,7 +132,6 @@ def getMomBinSyst(args):
     for runName, runFiles in fileSet.iteritems():
       for file in runFiles:
         bigTree.Add(file)
-        break
 
     print "  {0} events total".format(bigTree.GetEntries())
 
@@ -144,6 +152,350 @@ def getMomBinSyst(args):
       bin = None
       for key in keys:
         if entry.HMM_mom[0] >= key[0] and entry.HMM_mom[0] < key[1]:
+          bin = key
+          break
+
+      if bin:
+        buffSum = 0.
+        buffEntries = 0
+        for weight in weightBuff:
+          buffSum += weight
+          buffEntries += 1
+        buffMean = buffSum/buffEntries
+        buffDiff2Sum = 0.
+        for weight in weightBuff:
+          diff2 = (weight - buffMean)**2
+          buffDiff2Sum += diff2
+        buffVariance = buffDiff2Sum / buffEntries
+
+        # some systematics can be artificially inflated by errors in computing them - ignore these
+        if buffVariance < args.cutOff**2:
+          unweightedBinContents[bin] += 1
+          i = 0
+          for weight in weightBuff:
+            allToyBinContents[bin][i] += weight
+            i += 1
+
+    for key in keys:
+      print "  Bin from {0} to {1}".format(key[0], key[1])
+      # get bin content variance across toys
+      binContentSum = 0.
+      for i in range(1000):
+        binContentSum += allToyBinContents[key][i]
+      mean = binContentSum / 1000.
+
+      binContentDiff2Sum = 0.
+      for i in range(1000):
+        diff2 = (allToyBinContents[key][i] - mean)**2
+        binContentDiff2Sum += diff2
+      variance = binContentDiff2Sum / 1000.
+      sigma = math.sqrt(variance)
+
+      if mean:
+        print "  Total fractional systematic is {0}".format(sigma/mean)
+      print "  Final average bin content is {0} +- {1}".format(mean, sigma)
+      if unweightedBinContents[key]:
+        print "    as fraction of total is {0} +- {1}".format(mean/float(unweightedBinContents[key]), sigma/float(unweightedBinContents[key]))
+
+def getCosThetaBinSyst(args):
+  fileSets = getFileSets(args)
+
+  keys = []
+  allWeights = {}
+  allDiff2s = {}
+  allEvents = {}
+  for i in range(len(BINS_COSTHETA)-1):
+    low = BINS_COSTHETA[i]
+    up = BINS_COSTHETA[i+1]
+    keys.append((low, up))
+    allWeights[(low, up)] = []
+    allDiff2s[(low, up)] = []
+    allEvents[(low, up)] = 0
+
+  for key, fileSet in fileSets.iteritems():
+    print "--- Cos theta bin systematics for {0} ---".format(key)
+    print "  Using cut-off of {0}".format(args.cutOff)
+    bigTree = ROOT.TChain(TREE_SYS)
+    for runName, runFiles in fileSet.iteritems():
+      for file in runFiles:
+        bigTree.Add(file)
+
+    print "  {0} events total".format(bigTree.GetEntries())
+
+    allToyBinContents = {}
+    unweightedBinContents = {}
+    for key in keys:
+      allToyBinContents[key] = []
+      unweightedBinContents[key] = 0
+      for i in range(1000):
+        allToyBinContents[key].append(0.)
+
+    for entry in bigTree:
+      # ignore entries that haven't passed the selection
+      if entry.accum_level[0] < 11:
+        continue
+      weightBuff = entry.weight_syst_total
+
+      bin = None
+      for key in keys:
+        if entry.HMM_costheta >= key[0] and entry.HMM_costheta < key[1]:
+          bin = key
+          break
+
+      if bin:
+        buffSum = 0.
+        buffEntries = 0
+        for weight in weightBuff:
+          buffSum += weight
+          buffEntries += 1
+        buffMean = buffSum/buffEntries
+        buffDiff2Sum = 0.
+        for weight in weightBuff:
+          diff2 = (weight - buffMean)**2
+          buffDiff2Sum += diff2
+        buffVariance = buffDiff2Sum / buffEntries
+
+        # some systematics can be artificially inflated by errors in computing them - ignore these
+        if buffVariance < args.cutOff**2:
+          unweightedBinContents[bin] += 1
+          i = 0
+          for weight in weightBuff:
+            allToyBinContents[bin][i] += weight
+            i += 1
+
+    for key in keys:
+      print "  Bin from {0} to {1}".format(key[0], key[1])
+      # get bin content variance across toys
+      binContentSum = 0.
+      for i in range(1000):
+        binContentSum += allToyBinContents[key][i]
+      mean = binContentSum / 1000.
+
+      binContentDiff2Sum = 0.
+      for i in range(1000):
+        diff2 = (allToyBinContents[key][i] - mean)**2
+        binContentDiff2Sum += diff2
+      variance = binContentDiff2Sum / 1000.
+      sigma = math.sqrt(variance)
+
+      if mean:
+        print "  Total fractional systematic is {0}".format(sigma/mean)
+      print "  Final average bin content is {0} +- {1}".format(mean, sigma)
+      if unweightedBinContents[key]:
+        print "    as fraction of total is {0} +- {1}".format(mean/float(unweightedBinContents[key]), sigma/float(unweightedBinContents[key]))
+
+def getHotPathBinSyst(args):
+  fileSets = getFileSets(args)
+
+  keys = []
+  allWeights = {}
+  allDiff2s = {}
+  allEvents = {}
+  for i in BINS_MULT:
+    allWeights[i] = []
+    allDiff2s[i] = []
+    allEvents[i] = 0
+    keys.append(i)
+
+  for key, fileSet in fileSets.iteritems():
+    print "--- Hot path bin systematics for {0} ---".format(key)
+    print "  Using cut-off of {0}".format(args.cutOff)
+    bigTree = ROOT.TChain(TREE_SYS)
+    for runName, runFiles in fileSet.iteritems():
+      for file in runFiles:
+        bigTree.Add(file)
+
+    print "  {0} events total".format(bigTree.GetEntries())
+
+    allToyBinContents = {}
+    unweightedBinContents = {}
+    for key in keys:
+      allToyBinContents[key] = []
+      unweightedBinContents[key] = 0
+      for i in range(1000):
+        allToyBinContents[key].append(0.)
+
+    for entry in bigTree:
+      # ignore entries that haven't passed the selection
+      if entry.accum_level[0] < 11:
+        continue
+      weightBuff = entry.weight_syst_total
+
+      bin = None
+      for key in keys:
+        if entry.Vertex_N_hot_paths == key:
+          bin = key
+          break
+
+      if bin:
+        buffSum = 0.
+        buffEntries = 0
+        for weight in weightBuff:
+          buffSum += weight
+          buffEntries += 1
+        buffMean = buffSum/buffEntries
+        buffDiff2Sum = 0.
+        for weight in weightBuff:
+          diff2 = (weight - buffMean)**2
+          buffDiff2Sum += diff2
+        buffVariance = buffDiff2Sum / buffEntries
+
+        # some systematics can be artificially inflated by errors in computing them - ignore these
+        if buffVariance < args.cutOff**2:
+          unweightedBinContents[bin] += 1
+          i = 0
+          for weight in weightBuff:
+            allToyBinContents[bin][i] += weight
+            i += 1
+
+    for key in keys:
+      print "  Bin at {0}".format(key)
+      # get bin content variance across toys
+      binContentSum = 0.
+      for i in range(1000):
+        binContentSum += allToyBinContents[key][i]
+      mean = binContentSum / 1000.
+
+      binContentDiff2Sum = 0.
+      for i in range(1000):
+        diff2 = (allToyBinContents[key][i] - mean)**2
+        binContentDiff2Sum += diff2
+      variance = binContentDiff2Sum / 1000.
+      sigma = math.sqrt(variance)
+
+      if mean:
+        print "  Total fractional systematic is {0}".format(sigma/mean)
+      print "  Final average bin content is {0} +- {1}".format(mean, sigma)
+      if unweightedBinContents[key]:
+        print "    as fraction of total is {0} +- {1}".format(mean/float(unweightedBinContents[key]), sigma/float(unweightedBinContents[key]))
+
+def getHotProtonBinSyst(args):
+  fileSets = getFileSets(args)
+
+  keys = []
+  allWeights = {}
+  allDiff2s = {}
+  allEvents = {}
+  for i in BINS_MULT:
+    allWeights[i] = []
+    allDiff2s[i] = []
+    allEvents[i] = 0
+    keys.append(i)
+
+  for key, fileSet in fileSets.iteritems():
+    print "--- Hot proton bin systematics for {0} ---".format(key)
+    print "  Using cut-off of {0}".format(args.cutOff)
+    bigTree = ROOT.TChain(TREE_SYS)
+    for runName, runFiles in fileSet.iteritems():
+      for file in runFiles:
+        bigTree.Add(file)
+
+    print "  {0} events total".format(bigTree.GetEntries())
+
+    allToyBinContents = {}
+    unweightedBinContents = {}
+    for key in keys:
+      allToyBinContents[key] = []
+      unweightedBinContents[key] = 0
+      for i in range(1000):
+        allToyBinContents[key].append(0.)
+
+    for entry in bigTree:
+      # ignore entries that haven't passed the selection
+      if entry.accum_level[0] < 11:
+        continue
+      weightBuff = entry.weight_syst_total
+
+      bin = None
+      for key in keys:
+        if entry.Vertex_N_protons == key:
+          bin = key
+          break
+
+      if bin:
+        buffSum = 0.
+        buffEntries = 0
+        for weight in weightBuff:
+          buffSum += weight
+          buffEntries += 1
+        buffMean = buffSum/buffEntries
+        buffDiff2Sum = 0.
+        for weight in weightBuff:
+          diff2 = (weight - buffMean)**2
+          buffDiff2Sum += diff2
+        buffVariance = buffDiff2Sum / buffEntries
+
+        # some systematics can be artificially inflated by errors in computing them - ignore these
+        if buffVariance < args.cutOff**2:
+          unweightedBinContents[bin] += 1
+          i = 0
+          for weight in weightBuff:
+            allToyBinContents[bin][i] += weight
+            i += 1
+
+    for key in keys:
+      print "  Bin at {0}".format(key)
+      # get bin content variance across toys
+      binContentSum = 0.
+      for i in range(1000):
+        binContentSum += allToyBinContents[key][i]
+      mean = binContentSum / 1000.
+
+      binContentDiff2Sum = 0.
+      for i in range(1000):
+        diff2 = (allToyBinContents[key][i] - mean)**2
+        binContentDiff2Sum += diff2
+      variance = binContentDiff2Sum / 1000.
+      sigma = math.sqrt(variance)
+
+      if mean:
+        print "  Total fractional systematic is {0}".format(sigma/mean)
+      print "  Final average bin content is {0} +- {1}".format(mean, sigma)
+      if unweightedBinContents[key]:
+        print "    as fraction of total is {0} +- {1}".format(mean/float(unweightedBinContents[key]), sigma/float(unweightedBinContents[key]))
+
+def getMaxProtonMomBinSyst(args):
+  fileSets = getFileSets(args)
+
+  keys = []
+  allWeights = {}
+  allDiff2s = {}
+  allEvents = {}
+  for i in range(len(BINS_MOM)-1):
+    low = BINS_MOM[i]
+    up = BINS_MOM[i+1]
+    keys.append((low, up))
+    allWeights[(low, up)] = []
+    allDiff2s[(low, up)] = []
+    allEvents[(low, up)] = 0
+
+  for key, fileSet in fileSets.iteritems():
+    print "--- Highest proton momentum bin systematics for {0} ---".format(key)
+    print "  Using cut-off of {0}".format(args.cutOff)
+    bigTree = ROOT.TChain(TREE_SYS)
+    for runName, runFiles in fileSet.iteritems():
+      for file in runFiles:
+        bigTree.Add(file)
+
+    print "  {0} events total".format(bigTree.GetEntries())
+
+    allToyBinContents = {}
+    unweightedBinContents = {}
+    for key in keys:
+      allToyBinContents[key] = []
+      unweightedBinContents[key] = 0
+      for i in range(1000):
+        allToyBinContents[key].append(0.)
+
+    for entry in bigTree:
+      # ignore entries that haven't passed the selection
+      if entry.accum_level[0] < 11:
+        continue
+      weightBuff = entry.weight_syst_total
+
+      bin = None
+      for key in keys:
+        if entry.Vertex_max_proton_mom >= key[0] and entry.Vertex_max_proton_mom < key[1]:
           bin = key
           break
 
@@ -226,6 +578,10 @@ def checkArguments():
   parser = argparse.ArgumentParser(description="Produce plots for gas and beam")
   parser.add_argument("--singleBin", action="store_true", help="Compute systematics for single bin analysis")
   parser.add_argument("--momBin", action="store_true", help="Compute systematics for momentum bin analysis")
+  parser.add_argument("--costhetaBin", action="store_true", help="Compute systematics for cos theta bin analysis")
+  parser.add_argument("--hotPathBin", action="store_true", help="Compute systematics for high momentum path bin analysis")
+  parser.add_argument("--hotProtonBin", action="store_true", help="Compute systematics for high momentum proton bin analysis")
+  parser.add_argument("--maxProtonMomBin", action="store_true", help="Compute systematics for max proton momentum proton bin analysis")
 
   parser.add_argument("--neut", action="store_true", help="Use neut files")
   parser.add_argument("--genie", action="store_true", help="Use genie files")
